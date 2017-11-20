cb0f9d86e6290fd1cde4553124335f9fcd4cbc1d
Timofey P. Samodurov <timofey.samodurov@yandex.ru>
2017-11-20 11:35:27 +0000

    added models Phone, Email in main app, added base.html, index.html for sites root

--- main/models.py
+++ main/models.py
@@ -1,3 +1,28 @@
 from django.db import models
-
+from django.core.validators import RegexValidator,EmailValidator
 # Create your models here.
+
+class Phone(models.Model):
+    name = models.CharField(max_length=255, default='Номер телефона 1',unique=True,verbose_name="Название",help_text="")
+    number = models.CharField(max_length=15,unique=True,verbose_name="Номер телефона",help_text="Введите номер телефона используя следующий формат: +Х(ХХХ)ХХХ-ХХ-ХХ или X XXX XXX XX XX ",validators=[RegexValidator(regex=r'^(\+\d{1}|\d)(\ \d{3}\ |\(\d{3}\))(\d{3}\-\d{2}\-\d{2}|\d{3}\ \d{2}\ \d{2})$',)])
+    link = models.CharField(max_length=255,default="tel:",verbose_name="Ссылка",help_text="")
+    class Meta:
+        verbose_name = 'номер телефона'
+        verbose_name_plural = 'Номера телефонов'
+
+    def __str__(self):
+        return self.name+": "+self.number
+
+class Email(models.Model):
+    name = models.CharField(max_length=255, default='Адрес электронной почты 1',unique=True,verbose_name="Название")
+    email = models.CharField(max_length=255,unique=True,verbose_name="Адрес электронной почты (e-mail)",validators=[EmailValidator()])
+    link = models.CharField(max_length=255,default="mailto:",verbose_name="Ссылка")
+    class Meta:
+        verbose_name = 'Адрес электронной почты'
+        verbose_name_plural = 'Адреса электронной почты'
+
+    def __str__(self):
+        return self.name+": "+self.email
+
+#class Social(models.Model):
+#    fb_available = models.
--- tgs/views.py
+++ tgs/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
-
+from main.models import Phone,Email
 def home(request):
-    return render(request,'tgs/index.html')
+    phone = Phone.objects.get(name="Основной")
+    email = Email.objects.get(name="Основной")
+    context = {
+            'phone': phone,
+            'email': email,
+            }
+    return render(request,'tgs/index.html',context)
