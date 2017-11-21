2b976a50aa346e3134a78687011327a72cd7a0f5
Timofey P. Samodurov <timofey.samodurov@yandex.ru>
2017-11-21 09:43:35 +0000

    added dynamic urls to catalog menu template

--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    pass
-def catalog_list(request):
-    pass
+    return HttpResponse('Catalog') 
+def catalog_list(request,catalog_slug):
+    return HttpResponse("CATALOG: %s" % catalog_slug) 
+def category_list(request,category_slug,catalog_slug):
+    return HttpResponse("CATALOG: %s, CATEGORY %s " % (catalog_slug, category_slug,))
