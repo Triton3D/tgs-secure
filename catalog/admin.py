b820020f042b1903806669b072fddae7c94b55ff
Timofey P. Samodurov <timofey.samodurov@yandex.ru>
2017-11-21 08:00:07 +0000

    added catalog

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Catalog,Product,Category
 
-# Register your models here.
+admin.site.register(Catalog)
+admin.site.register(Product)
+admin.site.register(Category)
--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,45 @@
+# -*- coding:utf-8 -*-
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
-# Create your models here.
+
+@python_2_unicode_compatible
+class Catalog(models.Model):
+        name = models.CharField(max_length=255)
+        slug = models.SlugField(max_length=150)
+        description = models.TextField()
+
+        class Meta:
+            ordering = ('name',)
+
+        def __str__(self):
+            return self.name
+
+
+@python_2_unicode_compatible
+class Product(models.Model):
+        name = models.CharField(max_length=300)
+        slug = models.SlugField(max_length=150)
+        description = models.TextField()
+        manufacturer = models.CharField(max_length=300, blank=True)
+        price = models.DecimalField(max_digits=8, decimal_places=2)
+        available = models.BooleanField(default=True)
+
+        class Meta:
+            ordering = ('name',)
+
+        def __str__(self):
+            return self.name
+
+@python_2_unicode_compatible
+class Category(models.Model):
+        name = models.CharField(max_length=255)
+        slug = models.SlugField(max_length=255)
+        description = models.TextField(blank=True)
+        catalog = models.ManyToManyField(Catalog)
+
+        class Meta:
+            ordering = ('name',)
+
+        def __str__(self):
+            return self.name
