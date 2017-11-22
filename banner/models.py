d03e5fb7ed54607fdf3bb6b83371599727139fb7
Timofey P. Samodurov <lwrdrstv@gmail.com>
2017-11-22 15:44:26 +0500

    added img field to banner/models

--- banner/models.py
+++ banner/models.py
@@ -1,17 +1,23 @@
 from django.db import models
+#img1 = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
 
 class Banner(models.Model):
-    text1 = models.CharField(max_length=255,
-                            default='Баннер№№. Заголовок1', # user uppercase in template
-                            verbose_name="Баннер1. Заголовок1",
-                            help_text="some help text here")
+    text1 = models.CharField(max_length = 255,
+                            default = 'Заголовок1', # user uppercase in template
+                            verbose_name = "Заголовок1",
+                            help_text = "some help text here",
+                            blank = True)
 
-    text2 = models.CharField(max_length=255,
-                            default='Банер№№. Заголовок2', # user uppercase in template
-                            verbose_name="Баннер1. Заголовок2",
-                            help_text="some help text here")
+    text2 = models.CharField(max_length = 255,
+                            default = 'Заголовок2', # user uppercase in template
+                            verbose_name = "Заголовок2",
+                            help_text = "some help text here",
+                            blank = True)
 
-    button_text = models.CharField(max_length=255,
-                            default='Банер№№. Текст на кнопке', # use uppercase in template
-                            verbose_name="Баннер1. Текст на кнопке",
-                            help_text="show more  ") #use uppercase in template
+    button_text = models.CharField(max_length = 255,
+                            default = 'Текст на кнопке', # use uppercase in template
+                            verbose_name = "Текст на кнопке",
+                            help_text = "help text ",
+                            blank = True)
+
+    banner_img = models.ImageField(upload_to = 'products/%Y/%m/%d', blank=True)
