761da389957651c03c16543542c5c6664e8bd1f7
Timofey P. Samodurov <lwrdrstv@gmail.com>
2017-11-22 15:10:01 +0500

    added models to banner/models

--- banner/models.py
+++ banner/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Banner(models.Model):
+    text1 = models.CharField(max_length=255,
+                            default='Баннер№№. Заголовок1', # user uppercase in template
+                            verbose_name="Баннер1. Заголовок1",
+                            help_text="some help text here")
+
+    text2 = models.CharField(max_length=255,
+                            default='Банер№№. Заголовок2', # user uppercase in template
+                            verbose_name="Баннер1. Заголовок2",
+                            help_text="some help text here")
+
+    button_text = models.CharField(max_length=255,
+                            default='Банер№№. Текст на кнопке', # use uppercase in template
+                            verbose_name="Баннер1. Текст на кнопке",
+                            help_text="show more  ") #use uppercase in template
