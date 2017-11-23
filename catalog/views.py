69c9fba91d293ae671ebd18ce3d2d3fc30623239
usermuser <lwrdrstv@gmail.com>
2017-11-24 01:14:08 +0500

    configured catalogs template and catalog app view

--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from catalog.models import Catalog
+from catalog.models import Catalog, Category, Product
 
 # Create your views here.
 def index(request):
@@ -8,7 +8,15 @@ def index(request):
 
 def catalog_list(request,catalog_slug):
     catalogs = Catalog.objects.all()
-    context = {'catalogs' : catalogs,}
+    current_catalog = Catalog.objects.get(slug=catalog_slug)
+    categorys = Category.objects.all()
+    products = Product.objects.all()
+
+    context = {'catalogs' : catalogs,
+                'current_catalog' : current_catalog,
+                'categorys' : categorys,
+                'products' : products,
+                }
     return render (request,'catalog/catalog_list.html',context)
 
 def category_list(request,category_slug,catalog_slug):
