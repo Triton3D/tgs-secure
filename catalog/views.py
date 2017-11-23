4a3b9a2453f8d497e2c6552efeb5b5185d12611c
usermuser <lwrdrstv@gmail.com>
2017-11-24 00:15:05 +0500

    added catalot/catalog_list view

--- catalog/views.py
+++ catalog/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from catalog.models import Catalog
 
 # Create your views here.
 def index(request):
     return HttpResponse('Catalog') 
+
 def catalog_list(request,catalog_slug):
-    return HttpResponse("CATALOG: %s" % catalog_slug) 
+    catalogs = Catalog.objects.all()
+    context = {'catalogs' : catalogs,}
+    return render (request,'catalog/catalog_list.html',context)
+
 def category_list(request,category_slug,catalog_slug):
     return HttpResponse("CATALOG: %s, CATEGORY %s " % (catalog_slug, category_slug,))
