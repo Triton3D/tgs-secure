a9acb7a0ce4da59f21779661da3cd3ce181462e6
Timofey P. Samodurov <lwrdrstv@gmail.com>
2017-11-24 15:41:34 +0500

    Added category list template

--- catalog/urls.py
+++ catalog/urls.py
@@ -6,17 +6,10 @@ from . import views
 app_name = 'catalog'
 urlpatterns = [
             #    url(r'^$', views.product_list, name='product_list'),
-                url(r'^$',views.index,name='index'),
-                url(r'^(?P<catalog_slug>[a-zA-Z]+)/$',views.catalog_list,name='catalog_list'),
-                url(r'^(?P<catalog_slug>[a-zA-Z]+)/(?P<category_slug>[a-zA-Z]+)/$',views.category_list,name='category_list'),
-                url(r'^(?P<product_slug>product1)/$',views.product,name='product'),
+                url(r'^$',views.catalog_list,name='catalog_list'),
+                url(r'^(?P<catalog_slug>[a-zA-Z]+)/$',views.catalog_list,name='category_list'),
+                url(r'^(?P<category_slug>[a-zA-Z]+)/$',views.category_list,name='product_list'),
+                url(r'^(?P<product_slug>[a-zA-Z])/$',views.product,name='product'),
                 ]
-                    #ex: some url example
-                        #url ???, views.
-                            # # ex: /polls/
-                                # url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
-                                    # # ex: /polls/5/results/
-                                        # url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
-                                            # # ex: /polls/5/vote/
-                                                # url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+                   
                                                 
--- catalog/views.py
+++ catalog/views.py
@@ -1,18 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Product
+from .models import Product,Catalog,Category
 
 # Create your views here.
-def index(request):
-    return HttpResponse('Catalog') 
-def catalog_list(request,catalog_slug):
-    return HttpResponse("CATALOG: %s" % catalog_slug) 
-def category_list(request,category_slug,catalog_slug):
-    return HttpResponse("CATALOG: %s, CATEGORY %s " % (catalog_slug, category_slug,))
+def catalog_list(request):
+	catalog_list = Catalog.objects.all()
+	# product = Product.objects.all()
+	context = {
+	'catalog_list': catalog_list,
+	# 'product' : product,
+	}
+	return render(request,'catalog/index.html',context)
+
+def category_list(request,catalog_slug):
+	catalog = Catalog.objects.get(slug=catalog_slug)
+	category_list = Category.objects.get(catalog=catalog.name)
+	# product = Product.objects.get(catalog=catalog.name)
+	context = {'catalog': catalog,'category_list' : category_list,}
+	#'product' : product,
+	return render(request,'catalog/category_list.html',context)
+
+def product_list(request,category_slug):
+	category = Category.objects.get(slug=category_slug)
+	product_list = Product.objects.get(category=category.name)
+	context = {'category' : category,'product_list' : product_list,}
+	return render(request,'catalog/product.html',context)
+
 def product(request,product_slug):
 	product = Product.objects.get(slug=product_slug)
 	context = {
-	'product' : product,
-	}
+	'product' : product,}
 	return render(request,'catalog/product.html',context)
-	
+		
