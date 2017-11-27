c4e4035d4561d456a392c666d4f6797b55093311
usermuser <lwrdrstv@gmail.com>
2017-11-27 15:19:58 +0500

    chengad products preview in admin interface

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from .models import Catalog,Product,Category
 
+class ProductAdmin(admin.ModelAdmin):
+	list_display = ('name','category','slug','price','available','sku')
+#	list_display = ('first_name', 'last_name')
 admin.site.register(Catalog)
-admin.site.register(Product)
+admin.site.register(Product,ProductAdmin)
 admin.site.register(Category)
+
+
